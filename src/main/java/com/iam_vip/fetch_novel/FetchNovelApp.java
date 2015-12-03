f8af38ddff09cc4b0692842d61ae437d7ff79095
Colin Davis <nilocsivad@aol.com>
2015-12-03 18:14:39 +0800

    clear history

--- src/main/java/com/iam_vip/fetch_novel/FetchNovelApp.java
+++ src/main/java/com/iam_vip/fetch_novel/FetchNovelApp.java
@@ -6,38 +6,37 @@ import com.iam_vip.fetch_novel.biz.NovelFactory;
  * Hello world!
  */
 public class FetchNovelApp {
-
+	
 	/**
 	 * 
 	 */
-	public FetchNovelApp() {
-	}
-
+	public FetchNovelApp() {}
+	
 	public static void main( String[] args ) throws Exception {
-
+		
 		NovelFactory.setFolder( "/Users/Colin/Documents/fetch-novel" );
-
-		NovelFactory.createNovel( "http://www.23wx.com/html/51/51514/22464681.html", "造化之门2" ).fetch2html();
-		//		newThead2Fetch( new KV( "", "" ) );
-
+		
+		NovelFactory.createNovel( "", "" ).fetch2html();
+		// newThead2Fetch( new KV( "", "" ) );
+		
 	}
-
+	
 	public static void newThead2Fetch( KV... arr ) {
-
+		
 		for ( KV kv : arr ) {
 			new Thread() {
-
-				@Override
-				public void run() {
-
+				
+				@Override public void run() {
+					
 					try {
 						NovelFactory.createNovel( kv.url, kv.name ).fetch2html();
-					} catch ( Exception e ) {
+					}
+					catch ( Exception e ) {
 						e.printStackTrace();
 					}
 				}
 			}.start();
 		}
 	}
-
+	
 }
