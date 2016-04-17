fefda7e08c111d6d8be2117490f8affedeedcf58
Colin Davis <nilocsivad@aol.com>
2016-04-17 19:46:13 +0800

    linux mac windows

--- src/main/java/com/iam_vip/fetch_novel/FetchNovelApp.java
+++ src/main/java/com/iam_vip/fetch_novel/FetchNovelApp.java
@@ -10,55 +10,60 @@ import com.iam_vip.fetch_novel.biz.NovelFactory;
  * Hello world!
  */
 public class FetchNovelApp {
-	
+
 	/**
 	 * 
 	 */
-	public FetchNovelApp() {}
-	
-	public static void main( String[] args ) throws Exception {
-		
-		NovelFactory.setFolder( "D:\\Novel" );
-		
-		NovelFactory.novel_group_length = 100;
-		NovelFactory.createNovel( "", "" ).fetch( 100000 );
-		
-		// newThead2Fetch( new KV( "", "" ) );
-		
-		String url = "";
-		getHtml( url );
-		
+	public FetchNovelApp() {
 	}
-	
+
+	public static void main(String[] args) throws Exception {
+
+		if (System.getProperty("os.name").contains("windows")) {
+			NovelFactory.setFolder("D:\\Novel");
+		} else {
+			NovelFactory.setFolder(System.getProperty("user.home") + "/Novel/");
+		}
+
+		NovelFactory.novel_group_length = 100;
+		NovelFactory.createNovel("", "").fetch(100000);
+
+		// newThead2Fetch( new KV( "", "" ) );
+
+		String url = "";
+		getHtml(url);
+
+	}
+
 	/**
 	 * @param url
 	 * @throws IOException
 	 */
-	public static void getHtml( String url ) throws IOException {
-		
-		if ( url == null || "".equals( url ) ) return;
-		
-		String htm = Jsoup.connect( url ).timeout( 1000 * 60 * 3 ).get().html();
-		System.out.println( htm );
+	public static void getHtml(String url) throws IOException {
+
+		if (url == null || "".equals(url))
+			return;
+
+		String htm = Jsoup.connect(url).timeout(1000 * 60 * 3).get().html();
+		System.out.println(htm);
 	}
-	
-	public static void newThead2Fetch( KV... arr ) {
-		
-		for ( KV kv : arr ) {
+
+	public static void newThead2Fetch(KV... arr) {
+
+		for (KV kv : arr) {
 			new Thread() {
-				
+
 				@Override
 				public void run() {
-					
+
 					try {
-						NovelFactory.createNovel( kv.url, kv.name ).fetch2html();
-					}
-					catch ( Exception e ) {
+						NovelFactory.createNovel(kv.url, kv.name).fetch2html();
+					} catch (Exception e) {
 						e.printStackTrace();
 					}
 				}
 			}.start();
 		}
 	}
-	
+
 }
