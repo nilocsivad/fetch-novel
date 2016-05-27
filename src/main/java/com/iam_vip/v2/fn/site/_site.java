88cf57cfba93f4546e07bcf3b767b5199a141e76
Colin <nilocsivad@aol.com>
2016-05-27 21:09:15 +0800

    fetch header User-Agent...

--- src/main/java/com/iam_vip/v2/fn/site/_site.java
+++ src/main/java/com/iam_vip/v2/fn/site/_site.java
@@ -4,6 +4,7 @@
 package com.iam_vip.v2.fn.site;
 
 import java.io.IOException;
+import java.util.Random;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,8 +16,12 @@ import org.jsoup.select.Elements;
  */
 public interface _site {
 
+	String[] UserAgent = { "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:46.0) Gecko/20100101 Firefox/46.0", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)" };
+
 	default Document getDoc(String url) throws IOException {
-		return Jsoup.connect(url).timeout(1000 * 60 * 3).get();
+		int idx = new Random().nextInt(UserAgent.length);
+		String agent = UserAgent[idx];
+		return Jsoup.connect(url).header("Referer", url).header("User-Agent", agent).timeout(1000 * 60 * 3).get();
 	}
 
 	String getName(Document doc);
